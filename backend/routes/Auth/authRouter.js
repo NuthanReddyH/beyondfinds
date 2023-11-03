a4ff27a785ffdb8e3101308d868ffa1c578125e1
NuthanreddyH <Nuthanreddy.t@gmail.com>
2023-11-03 17:09:53 -0400

    Edit Profile Integration

--- backend/routes/Auth/authRouter.js
+++ backend/routes/Auth/authRouter.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const authRouter = express.Router();
-const { login, register } = require('../../controllers/authController');
+const { login, register,updateUser } = require('../../controllers/authController');
 
 authRouter.post('/login', login);
 authRouter.post('/register', register);
+authRouter.put('/update', updateUser); 
 
 module.exports = authRouter;
--- frontend/src/components/Account/Account.js
+++ frontend/src/components/Account/Account.js
@@ -1,26 +1,102 @@
-import React, { useState } from 'react';
-import { Avatar, Button, TextField, Container, Grid, Typography } from '@mui/material';
+import React, { useState, useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux'; 
+import { updateUserThunk } from '../../data/authSlice';
+import './Account.css';
+import {
+  Avatar,
+  Button,
+  TextField,
+  Box,
+  Tabs,
+  Tab,
+  Typography
+} from '@mui/material';
 import PhotoCameraIcon from '@mui/icons-material/PhotoCamera';
 
-const userData = {
-    username: 'JohnDoe',
-    email: 'johndoe@example.com',
-    phoneNumber: '123-456-7890',
-  };
-
 function Profile() {
+  const dispatch = useDispatch();
+  //const { user } = useSelector((state) => state.auth);
+  const user = JSON.parse(localStorage.getItem('userInfo'));
+  
   const [profilePicture, setProfilePicture] = useState(null);
   const [isEditing, setIsEditing] = useState(false);
+  const [selectedTab, setSelectedTab] = useState(0);
+  
+  
+  const [username, setUsername] = useState(user?.username || '');
+  const [email, setEmail] = useState(user?.email || '');
+  const [phoneNumber, setPhoneNumber] = useState(user?.phoneNumber || '');
+
+
+  useEffect(() => {
+    if(user) {
+      setUsername(user.username);
+      setEmail(user.email);
+      setPhoneNumber(user.phoneNumber);
+    }
+  }, [user]);
 
   const handleFileChange = (e) => {
     const file = e.target.files[0];
     setProfilePicture(file);
   };
 
+  const handleTabChange = (event, newValue) => {
+    setSelectedTab(newValue);
+  };
+
+  const handleSaveChanges = async () => {
+    const updatedData = {
+      username,
+      email,
+      phoneNumber,
+    };
+
+
+    dispatch(updateUserThunk(updatedData));
+    setIsEditing(false);
+  };
+
+  const TabPanel = (props) => {
+    const { children, value, index, ...other } = props;
+    return (
+      <div
+        role="tabpanel"
+        hidden={value !== index}
+        id={`vertical-tabpanel-${index}`}
+        aria-labelledby={`vertical-tab-${index}`}
+        {...other}
+      >
+        {value === index && (
+          <Box>
+            {children}
+          </Box>
+        )}
+      </div>
+    );
+  };
+
   return (
-    <Container maxWidth="sm">
-      <Grid container spacing={2} justifyContent="center">
-        <Grid item>
+    <Box sx={{ flexGrow: 1, display: 'flex', height: '100%', marginTop: '20px' }}>
+      <Tabs
+        orientation="vertical"
+        variant="scrollable"
+        value={selectedTab}
+        onChange={handleTabChange}
+        sx={{ borderRight: 1, borderColor: 'divider' }}
+      >
+        <Tab label="Edit Profile" />
+        <Tab label="Change Password" />
+        <Tab label="Manage Membership" />
+      </Tabs>
+
+      <TabPanel value={selectedTab} index={0} className="tabpanel">
+        <Box sx={{ p: 3 }}>
+          <Avatar
+            alt="Profile Picture"
+            src={profilePicture ? URL.createObjectURL(profilePicture) : ''}
+            sx={{ width: 100, height: 100 }}
+          />
           <label htmlFor="profile-picture-input">
             <input
               type="file"
@@ -34,54 +110,50 @@ function Profile() {
               component="span"
               startIcon={<PhotoCameraIcon />}
             >
-              Upload Profile Picture
+              Upload
             </Button>
           </label>
-        </Grid>
-        <Grid item>
-          <Avatar
-            alt="Profile Picture"
-            src={profilePicture ? URL.createObjectURL(profilePicture) : ''}
-            sx={{ width: 120, height: 120 }}
+          <TextField
+            fullWidth
+            margin="normal"
+            label="Username"
+            value={username}
+            onChange={e => setUsername(e.target.value)}
+            InputProps={{
+              readOnly: true,
+            }}
           />
-        </Grid>
-      </Grid>
-      <TextField
-        fullWidth
-        margin="normal"
-        label="Username"
-        value={userData.username}
-        InputProps={{
-          readOnly: !isEditing,
-        }}
-      />
-      <TextField
-        fullWidth
-        margin="normal"
-        label="Email Address"
-        value={userData.email}
-        InputProps={{
-          readOnly: !isEditing,
-        }}
-      />
-      <TextField
-        fullWidth
-        margin="normal"
-        label="Phone Number"
-        value={userData.phoneNumber}
-        InputProps={{
-          readOnly: !isEditing,
-        }}
-      />
-      <Button
-        variant="contained"
-        color="primary"
-        disabled={!isEditing}
-        onClick={() => setIsEditing(false)}
-      >
-        Save Changes
-      </Button>
-    </Container>
+          <TextField
+            fullWidth
+            margin="normal"
+            label="Email Address"
+            value={email}
+            onChange={e => setEmail(e.target.value)}
+            
+          />
+          <TextField
+            fullWidth
+            margin="normal"
+            label="Phone Number"
+            value={phoneNumber}
+            onChange={e => setPhoneNumber(e.target.value)}
+            
+          />
+          
+            <Button
+              variant="contained"
+              className='btn-save'
+              onClick={handleSaveChanges}
+            >
+              Save Changes
+            </Button>
+          
+        </Box>
+      </TabPanel>
+      
+      {/* ... other TabPanels for Change Password, My Listing, Manage Membership ... */}
+      
+    </Box>
   );
 }
 
