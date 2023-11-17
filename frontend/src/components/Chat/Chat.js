c02f3fc83b5a70b8586d8e51a042322b6c59c281
NuthanreddyH <Nuthanreddy.t@gmail.com>
2023-11-17 14:31:51 -0500

    fix error in messages

--- frontend/src/components/Chat/Chat.js
+++ frontend/src/components/Chat/Chat.js
@@ -1,23 +1,22 @@
-import React, { useState, useEffect, useRef } from 'react';
-import { useParams } from 'react-router-dom';
-import { io } from 'socket.io-client';
-import './Chat.css';
-import { useSelector } from 'react-redux';
+import React, { useState, useEffect, useRef } from "react";
+import { useParams } from "react-router-dom";
+import { io } from "socket.io-client";
+import "./Chat.css";
+import { useSelector } from "react-redux";
 
 const Chat = () => {
   const { sellerName, username } = useParams();
-  console.log({sellerName})
   const user = useSelector((state) => state.auth.user);
-  console.log({user})
+  console.log({ user });
   const userId = user?._id;
-  const [message, setMessage] = useState('');
+  const [message, setMessage] = useState("");
   const [messages, setMessages] = useState([]);
   const [socket, setSocket] = useState(null);
   const scrollRef = useRef();
 
   useEffect(() => {
     // Connect to Socket.io server
-    const newSocket = io('http://localhost:8080'); // Replace with your server URL
+    const newSocket = io("http://localhost:8080"); // Replace with your server URL
 
     setSocket(newSocket);
 
@@ -31,10 +30,10 @@ const Chat = () => {
 
   useEffect(() => {
     // Connect to Socket.io server
-    const newSocket = io('http://localhost:8080'); // Replace with your server URL
-  
+    const newSocket = io("http://localhost:8080"); // Replace with your server URL
+
     setSocket(newSocket);
-  
+
     // Clean up on component unmount
     return () => {
       if (newSocket) {
@@ -42,35 +41,42 @@ const Chat = () => {
       }
     };
   }, []);
-  
+
   useEffect(() => {
     const fetchData = async () => {
       try {
         // Fetch chat history from the server
-        const response = await fetch(`http://localhost:8080/api/chat/getMessages/${username}/${sellerName}`);
+        const response = await fetch(
+          `http://localhost:8080/api/chat/getMessages/${username}/${sellerName}`
+        );
+        console.log({ response });
         const data = await response.json();
-        console.log(data);
-        // Update the messages state with the fetched chat history
-        setMessages(data);
+        console.log({ data });
+        if (data.error) {
+          setMessages([]);
+        } else {
+          // Update the messages state with the fetched chat history
+          setMessages(data);
+        }
       } catch (error) {
-        console.error('Error fetching chat history:', error);
+        console.error("Error fetching chat history:", error);
       }
     };
-  
+
     // Ensure the socket is connected
     if (socket && sellerName && username) {
       // Join a room based on the sellerName and username
-      socket.emit('join', { senderId: username, receiverId: sellerName });
-  
+      socket.emit("join", { senderId: username, receiverId: sellerName });
+
       // Listen for received messages
-      socket.on('receiveMessage', (data) => {
+      socket.on("receiveMessage", (data) => {
         setMessages((prevMessages) => [...prevMessages, data.message]);
       });
-  
+
       // Fetch chat history when the component mounts
       fetchData();
     }
-  
+
     // Clean up socket on component unmount
     return () => {
       if (socket) {
@@ -78,12 +84,11 @@ const Chat = () => {
       }
     };
   }, [socket, sellerName, username]);
-  
 
   const sendMessage = () => {
-    if (socket && message.trim() !== '') {
+    if (socket && message.trim() !== "") {
       // Emit the message to the server
-      socket.emit('sendMessage', {
+      socket.emit("sendMessage", {
         senderId: username,
         receiverId: sellerName,
         message: { text: message },
@@ -96,16 +101,17 @@ const Chat = () => {
       // ]);
 
       // Clear the input field
-      setMessage('');
+      setMessage("");
     }
   };
 
   const isSender = (sender) => sender === username;
 
   useEffect(() => {
-    scrollRef.current?.scrollIntoView({ behavior: 'smooth' });
+    scrollRef.current?.scrollIntoView({ behavior: "smooth" });
   }, [messages]);
 
+  console.log({ messages });
   return (
     <div className="messenger">
       <div className="chatBoxTop">
@@ -113,9 +119,15 @@ const Chat = () => {
           <div
             key={index}
             ref={scrollRef}
-            className={isSender(msg.sender === userId ? user?.username : msg.sender) ? 'messageSender' : 'messageReceiver'}
+            className={
+              isSender(msg.sender === userId ? user?.username : msg.sender)
+                ? "messageSender"
+                : "messageReceiver"
+            }
           >
-            <strong>{msg.sender === userId ? user?.username : msg.sender}: </strong>
+            <strong>
+              {msg.sender === userId ? user?.username : msg.sender}:{" "}
+            </strong>
             {msg.content}
           </div>
         ))}
